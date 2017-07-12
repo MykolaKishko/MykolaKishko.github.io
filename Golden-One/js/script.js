dee41eea7674a7bd3dee571473aae6a0ee36f063
MykolaKishko <ac672169@gmail.com>
2017-07-12 18:12:10 +0300

    саи
    ваи

--- Golden-One/js/script.js
+++ Golden-One/js/script.js
@@ -1,16 +1,79 @@
-    
-    $('a[href^="#"]').click(function () {
-         elementClick = $(this).attr("href");
-         destination = $(elementClick).offset().top;
-         if($.browser.safari){
-         $('body').animate( { scrollTop: destination }, 1000 );
-         } else {
-             $('html').animate( { scrollTop: destination }, 1000 );
-         }
-         return false;
-    });
+// Фіксований HEADER
+$(document).ready(function(){
+			// Фикмированная шапка при скролле
+			$("#header").removeClass("default");
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 20) {
+					$("#header").addClass("default").fadeIn('fast');
+				} else {
+					$("#header").removeClass("default").fadeIn('fast');
+				};
+			});
+		});
 
- 
+// Плавний скрол по якорях
+$(document).ready(function(){
+	$("#nav").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top - 75;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1200);
+	});
+});
+
+
+
+
+
+
+
+$(document).ready(function(){
+			// Фикмированная шапка при скролле
+			$("#arr").removeClass("block");
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 500) {
+					$("#arr").addClass("block").fadeIn('fast');
+				} else {
+					$("#arr").removeClass("block").fadeOut('fast');
+				};
+			});
+		});
+
+
+$(document).ready(function(){
+	$("#arr").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top - 75;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
+
+
+$(document).ready(function(){
+    
+    
+    $("html").niceScroll({cursorcolor: '#FED136', cursoropacitymin: '0' });
+    });
+                  
+
+
+
 
 
 
